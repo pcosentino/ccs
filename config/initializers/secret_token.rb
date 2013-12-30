acd577f7a5969dc797301e37e313f302f42c0d90
Paolo Cosentino <cosepaol88@gmail.com>
2013-12-30 15:13:53 -0600

    Initial Promotion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ccs::Application.config.secret_key_base = 'e9cf0e16ce333ee2ffac51535b23b3df219741397560610d06122073ef431b7bcdfad9e2d175f0259e15c325a12cde70e3689fea25d37889f136373d3ee2def0'
