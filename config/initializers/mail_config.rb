10ff55cc9e5a090f99bfd5103709c06a3195a986
Paolo Cosentino <cosepaol88@gmail.com>
2014-03-07 16:24:31 -0600

    adjust logos

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-     :address              => "smtp.gmail.com",
+     :address              => "smtp.com",
      :port                 => 587,
      :domain               => "gmail.com",
      :user_name            => 'office@completecustom.net',
