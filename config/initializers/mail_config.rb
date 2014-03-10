b4452ac2f5c8aed8617a100ab3d4d704ed4f84d5
Paolo Cosentino <cosepaol88@gmail.com>
2014-03-10 12:17:46 -0500

    update 3

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
      :address              => "smtp.gmail.com",
      :port                 => 587,
-     :domain               => "gmail.com",
+     :domain               => "google.com",
      :user_name            => 'office@completecustom.net',
      :password             => 'cosmetology!1',
      :authentication       => 'plain',
